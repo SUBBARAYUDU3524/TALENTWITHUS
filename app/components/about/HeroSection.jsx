3605dc456949796582ae021060bb33826c7ff466
rvenkateswarreddy <rvenkateswarreddy12345@gmail.com>
2025-11-11 02:38:21 +0530

    udpate about

--- app/components/about/HeroSection.jsx
+++ app/components/about/HeroSection.jsx
@@ -12,7 +12,7 @@ export default function HeroSection({ onScrollTo }) {
       <div className="relative z-10 w-full flex flex-col items-center justify-center px-4">
         <h1 className="text-center font-black leading-tight mb-6 text-5xl md:text-7xl tracking-tight">
           <span className="block text-[2.8rem] sm:text-5xl md:text-6xl lg:text-7xl mb-2 font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#1EB8F3] via-[#00AEEF] to-[#0059FF] drop-shadow-lg">
-            About
+            About Us
           </span>
           <span className="block text-white text-[2.5rem] sm:text-[3.5rem] md:text-6xl lg:text-7xl mb-2 font-black drop-shadow-lg">
             Talent With Us
